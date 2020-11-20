dcb2b00b691fa2080de84903458ee5659653c68c
maixuanlinh <maixuanlinhdkh@gmail.com>
2020-11-21 01:38:33 +0200

    Initial Commit

---
+++ TipsyApp_ByMXL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipsyApp_ByMXL
+//
+//  Created by Mai Xuan Linh on 21.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
